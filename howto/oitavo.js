0be11d2564e9b98113b971968d6d971ff824783d
Nicolas França <nicolasfranca9@gmail.com>
2014-06-09 14:22:07 -0300

    Melhorias, exercício 8

--- howto/oitavo.js
+++ howto/oitavo.js
@@ -8,7 +8,8 @@ function titleize(text) {
   var words = word.split(" ");
   var title = "";
   for (var i = 0; i < words.length; i++)
-    words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length);
+    words[i] = words[i].substring(0, 1).toUpperCase() +
+               words[i].substring(1, words[i].length);
   for (var i = 0; i < words.length; i++)
     title += words[i] + " ";
   return title;
