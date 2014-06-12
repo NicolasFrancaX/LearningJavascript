6af1d8a9e48444c0b62c972cd64896c130c6b6f5
Nicolas França <nicolasfranca9@gmail.com>
2014-06-12 16:50:56 -0300

    Melhorando implementação - Números primos

--- howto/decimo.js
+++ howto/decimo.js
@@ -1,16 +1,24 @@
-// Escreva uma função primes(start, end) que retorne um array 
 // contendo os números primos deste intervalo. Números primos 
+// Escreva uma função primes(start, end) que retorne um array 
 // são aqueles maiores que 1 e divisíveis apenas por 1 e ele mesmo.
 
 function prime(n) {
-  for (var i = 2; i !== n; i++)
-    if (n === 1 || n % i === 0) return false;
+  for (var i = 2; i <= Math.sqrt(n); i++) {
+    if (n === 1 || n % i === 0) {
+      return false;
+    }
+  }
   return true;
 }
 
 function primes(start, end) {
   var numbers = [];
-  for (var i = start; i <= end; i++)
-    if (prime(i)) numbers.push(i);
+  for (var i = start; i <= end; i++) {
+    if (prime(i)) {
+      numbers.push(i);
+    }
+  }
   return numbers;
 }
+
+console.log(primes(1, 5));
