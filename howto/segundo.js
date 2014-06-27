a7d56dafb3079ecc53ac179bfc3b4d389c47476a
Nicolas França <nicolasfranca9@gmail.com>
2014-06-27 14:57:13 -0300

    segundo.js: Usando função anônima.

--- howto/segundo.js
+++ howto/segundo.js
@@ -1,10 +1,4 @@
 // Altere a função max() para receber qualquer quantidade de 
 // argumentos e retornar o maior número dentre eles.
 
-function max() {
-  var maximo = arguments[0];
-  for (var i = 1; i < arguments.length; i++) {
-    maximo = Math.max(maximo, arguments[i]);
-  }
-  return maximo;
-}
+var max = Math.max;
