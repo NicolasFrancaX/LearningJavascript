bf8ce99c165c4dd949f07849d02a6924f7a8ef9e
Nicolas França <nicolasfranca9@gmail.com>
2014-06-30 11:53:47 -0300

    Refatorando

--- howto/quinto.js
+++ howto/quinto.js
@@ -5,9 +5,11 @@
 
 function findLongestWord() {
   var largerWord = arguments[0];
+
   for (var i = 1; i < arguments.length; i++) {
     largerWord = largerWord.length <= arguments[i].length ?
                  arguments[i] : largerWord;
   }
+
   return largerWord;
 }
--- howto/setimo.js
+++ howto/setimo.js
@@ -3,9 +3,9 @@
 
 function letterFrequence(text) {
   var frequence = {};
-  
+
   for (var i = 0; i < text.length; i++) {
-    if (frequence[text[i]] === undefined) {
+    if (!frequence[text[i]]) {
       frequence[text[i]] = 0;
     }
     frequence[text[i]]++;
